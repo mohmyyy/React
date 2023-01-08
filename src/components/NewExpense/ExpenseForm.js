f4aaa8c919bce3d5132fe16d963cef2c72eed935
mohmyyy <iammohmy@gmail.com>
2023-01-08 14:16:53 +0530

    task 7

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,10 +2,51 @@ import React,{useState} from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = ()=>{
-  const titleChangeHandler = (event) => {
-    console.log(event.target.value)
+  const [enteredTitle ,setEnteredTitle] = useState('');
+  const [enteredAmount,setEnteredAmount] = useState('');
+  const [enteredDate,setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // })
 
+    // State for title
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value)
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // })
   }
+  // State for Amount
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+    //     setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // })
+  }
+    // State for Date
+
+    const dateChangeHandler = (event) => {
+      setEnteredDate(event.target.value)
+      // setUserInput((prevState) => {
+      //   return {...prevState,
+      //   enteredDate: event.target.value}
+      // })
+    }
+
+    const submitEventHandler = (event)=>{
+      event.preventDefault()
+      const myObj = {
+        title:document.getElementById('titleId').value,
+        amount:document.getElementById('amountId').value,
+        date:document.getElementById('dateId').value
+      }
+      console.log(myObj)
+    }
   return <form>
     <div className='new-expense__controls'>
       <div className='new-expense__control'>
@@ -14,15 +55,15 @@ const ExpenseForm = ()=>{
       </div>
       <div className='new-expense__control'>
         <label> Amount </label>
-        <input id='amountId' type='number' min="0.1" step="0.01"/>
+        <input onChange={amountChangeHandler} id='amountId' type='number' min="0.1" step="0.01"/>
       </div>
       <div className='new-expense__control'>
         <label> Date </label>
-        <input id='dateId' type='date' min="2019-01-01" max="2023-12-31"/>
+        <input onChange={dateChangeHandler} id='dateId' type='date' min="2019-01-01" max="2023-12-31"/>
       </div>
     </div>
     <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
+        <button type='submit' onClick={submitEventHandler} >Add Expense</button>
       </div>
   </form>
 }
