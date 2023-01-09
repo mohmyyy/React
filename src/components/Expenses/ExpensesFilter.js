27458d061c4af38cf14e6777b1939ca160a6dba2
mohmyyy <iammohmy@gmail.com>
2023-01-09 17:24:10 +0530

    task 11

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -4,7 +4,7 @@ import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
   const dropDownChangeHandler = (event)=> {
-    props.oncChangeFilter(event.target.value)
+    props.onChangeFilter(event.target.value)
 
   }
   return (
@@ -12,6 +12,7 @@ const ExpensesFilter = (props) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropDownChangeHandler} >
+          <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
